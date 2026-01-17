3238c060a2da1ab2ced6d280a60923af5bed1fd2
Ranger <mnipk1243@gmail.com>
2026-01-17 19:48:47 +0500

    changes in routes file, main file

--- main.py
+++ main.py
@@ -1,35 +1,11 @@
-from fastapi import FastAPI, HTTPException, Response
+from fastapi import FastAPI, HTTPException, Request
 from dotenv import load_dotenv
-from auth.auth import Authentication
-from database.authorizationDB import mongo_db_auth_connector
-from database.redis_db import redis_connector
+from routes import auth_routes
 
 load_dotenv()
 
 app = FastAPI()
-auth_dbconnector = mongo_db_auth_connector()
-redis_dbconnector = redis_connector()
-auth_obj = Authentication(auth_dbconnector, redis_dbconnector)
 
-@app.post("/signup")
-async def signup(username: str, password: str, email: str):
-    status = auth_obj.sign_up(username, password, email)
-    if status["Exception"] is not None:
-        raise HTTPException(status_code=401, detail=f"Account Not Registered.")
-    return status
+app.include_router(auth_routes.router)
 
-@app.post("/login")
-async def Login(username: str, password: str, response: Response):
-    resp = auth_obj.login(username, password)
-    if resp["status"] == False:
-        raise HTTPException(status_code=401, detail=f"Failed To Login.")
-
-    session_id = resp["session_id"]
-    response.set_cookie(
-        key="session_id",
-        value=session_id,
-        httponly=True,
-        secure=True,
-        samesite="lax"
-    )
-    return {"status": "successful"}
+    
